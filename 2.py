a27dd88c88c03aa790bc8dba617858f3c735fce0
mukullokhande99 <2016bec077@sggs.ac.in>
2021-01-08 23:08:32 +0530

    Solution 2 added

--- 2.py
+++ 2.py
@@ -1,13 +1,15 @@
 #!/bin/python3
+import sys
 
-num =int(input())
-for i in range(num):
-    a= int(input())
-    p =[1, 2];
-    r =2
-    while(p[-1]+p[-2]<=a):
-        p.append(p[-2]+p[-1])
-        if(p[-1]%2 == 0):
-            r+=p[-1];
-    print(r)
+arr = int(input().strip())
+for i in range(arr):
+    num = int(input().strip())
+    total_sum = 0
+    p = 0
+    q = 1
+    while q < num:
+        if q%2==0:    #even
+            total_sum += q
+        p,q = q,p+q
+    print(total_sum)
     
